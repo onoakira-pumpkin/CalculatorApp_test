e916bbf49972e971f05829c74a721526008e086b
小野瞭 <onoakira@onoakiranoMacBook-Air.local>
2020-03-10 17:52:17 +0900

    initial commi

---
+++ CclculatorApp_test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CclculatorApp_test
+//
+//  Created by 小野瞭 on 2020/03/10.
+//  Copyright © 2020 akira.ono. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
